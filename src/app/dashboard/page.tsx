50e3b6fa2353651e183ab728f75571094da66f91
Bond - Tran Le Bao Loc <bond@vinova.com.sg>
2023-04-09 03:32:06 +0700

    initial setup codebase

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { FC } from 'react';
+
+import Button from '@/components/ui/Button';
+
+interface IDashboardProps {}
+
+const Dashboard: FC<IDashboardProps> = ({}) => {
+  return (
+    <div>
+      <Button>Test Button</Button>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,52 @@
+import React, { ButtonHTMLAttributes } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { Loader2 } from 'lucide-react';
+
+import { cn } from '@/lib/utils';
+
+export const buttonVariants = cva(
+  'active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-all focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none',
+  {
+    variants: {
+      variant: {
+        default: 'bg-slate-900 text-white hover:bg-slate-800',
+        ghost: 'bg-transparent hover:text-slate-900 hover:bg-slate-200',
+      },
+      size: {
+        default: 'h-10 py-2 px-4',
+        sm: 'h-9 px-2',
+        lg: 'h-11 px-8',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+interface IButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  ({ className, size, variant, isLoading, children, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        {...props}
+        disabled={isLoading}
+        className={cn(buttonVariants({ size, variant, className }))}
+      >
+        {isLoading && <Loader2 className="w-4 h-4 mr-2 animate-spin" />}
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = 'Button';
+
+export default Button;
